f51633ea255059a6467ca036bc06ba0c74c620f5
jherkyesse <jherkyesse@gmail.com>
2017-11-21 15:00:38 +0800

    Add files via upload

--- js/menuInit.js
+++ js/menuInit.js
@@ -1,30 +1,36 @@
-document.getElementById('nav-menu-icon').addEventListener('click',function(){
-    /* init */
-    document.querySelector('.navbar-menu ul li').style.marginTop = '0'
-    document.getElementById('nav-submenu').style.display = 'none'
-    for(var i=0;i<6;i++){
-        document.querySelectorAll('li#all-wine-dropmenu')[i].style.display = 'block'
-        document.querySelectorAll('li#all-wine-dropmenu')[i].children[2].style.display = 'none'
-        document.querySelectorAll('li#all-wine-dropmenu')[i].children[1].style.transform = 'rotate(90deg)'
+var allwinemenuclickCount=0;
+var allwinedropmenuclickCount=0
+
+document.getElementById('navmenustart').addEventListener('click',function(){
+
+    document.getElementById('navmenuclose').onclick=function(){
+        document.getElementById('nav-menu-icon').classList.toggle('navbar-menu-span')
+        document.getElementById('nav-menu').classList.toggle('hidden-xs')
     }
-    /* init */
-    document.getElementById('all-wine-menu').onclick = function(){
-        document.getElementById('nav-submenu').style.display = 'block'
-        document.querySelector('.navbar-menu ul li').style.marginTop = '-60px'
 
+    document.getElementById('all-wine-menu').onclick = function () {
+        allwinemenuclickCount == 0 ? allwinemenuclickCount++ : allwinemenuclickCount++
+        allwinemenuclickCount == 1 ? document.getElementById('nav-submenu').classList.toggle('hidden-xs') : ''
+        allwinemenuclickCount == 0 ? document.querySelector('.navbar-menu ul li').style.marginTop = '0' : document.querySelector('.navbar-menu ul li').style.marginTop = '-60px'
+        document.getElementById('nav-menu-icon').classList.add('navbar-menu-span-all')
+        document.querySelector('#nav-menu-icon > img:nth-child(3)').onclick = function () {
+            allwinemenuclickCount = 0
+            allwinedropmenuclickCount = 0
+            allwinemenuclickCount == 0 ? document.getElementById('nav-submenu').classList.toggle('hidden-xs') : ''
+            allwinemenuclickCount == 0 ? document.querySelector('.navbar-menu ul li').style.marginTop = '0' : ''
+            document.getElementById('nav-menu-icon').classList.toggle('navbar-menu-span-all')
+        }
 
-            Array.prototype.map.call(document.querySelectorAll('li#all-wine-dropmenu'),function(element,index){
-                element.onclick = function(){
+        Array.prototype.map.call(document.querySelectorAll('li#all-wine-dropmenu'), function (element, index) {
+            element.onclick = function () {
                 for(var i=0;i<6;i++){
-                    document.querySelector('#nav-submenu').children[i].children[2].style.display = 'none'
-                    document.querySelectorAll('li#all-wine-dropmenu')[i].style.display = 'none'
+                    i==index ? "" : document.querySelectorAll('li#all-wine-dropmenu')[i].children[2].classList.add('hidden-xs')
                 }
-                document.querySelectorAll('li#all-wine-dropmenu')[index].children[2] ? document.querySelectorAll('li#all-wine-dropmenu')[index].children[2].style.display = 'block' : ""
-                document.querySelectorAll('li#all-wine-dropmenu')[index].children[1] ? document.querySelectorAll('li#all-wine-dropmenu')[index].children[1].style.transform = 'rotate(-90deg)' : ""
-                document.querySelectorAll('li#all-wine-dropmenu')[index].style.display = 'block'
+                allwinedropmenuclickCount == 0 ? allwinedropmenuclickCount++ : allwinedropmenuclickCount--
+                document.querySelectorAll('li#all-wine-dropmenu')[index].children[2] ? document.querySelectorAll('li#all-wine-dropmenu')[index].children[2].classList.toggle('hidden-xs') : ""
+                document.querySelectorAll('li#all-wine-dropmenu')[index].children[1] && allwinedropmenuclickCount == 1 ? document.querySelectorAll('li#all-wine-dropmenu')[index].children[1].style.transform = 'rotate(-90deg)' : document.querySelectorAll('li#all-wine-dropmenu')[index].children[1].style.transform = 'rotate(90deg)'
             }
-            })
-
+        })
     }
     document.getElementById('nav-menu-icon').classList.toggle('navbar-menu-span')
     document.getElementById('nav-menu').classList.toggle('hidden-xs')
@@ -49,4 +55,7 @@ Array.prototype.map.call(document.querySelectorAll('.cart-list-item-add'),functi
 document.querySelector('.cart-list-item-all-delete-btn').onclick=function(){
     document.querySelector('.cart-list-item-box-block').innerHTML=''
     document.getElementById('navbar-cart-count').innerHTML=''
+}
+document.querySelector('.navbar-cart > img:nth-child(3)').onclick=function(){
+    document.querySelector('.cart-list').classList.toggle('hidden')
 }
